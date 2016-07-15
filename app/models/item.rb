db606355d3d890b9771844d58ceeeddb7ce94e9f
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-15 14:33:37 -0400

    item.rb, category.rb, add has_and_belongs_to_many assocaition

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
 	belongs_to :seller, :class_name => 'User', :foreign_key => 'seller_id'
+	has_and_belongs_to_many :categories
 	enum status: {available: 0, pending: 1, sold: 2, expired: 3, banned: 4 }
 	scope :category, -> (category) { where category: category}
 
