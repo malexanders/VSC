eeabaa715b2fdeb5d3b55077cdc4e2934b587122
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-15 17:08:32 -0400

    users_controller_spec, require 'spec_helper'; refactor using let! to create user and before {...} for the API call; add spec to verify get request reponds with content_type 'application/json'

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe UsersController, type: [:controller] do
+	let!(:user) { FactoryGirl.create(:user) }
 
-  describe "GET #items" do
-    it "returns http success" do
-			user = FactoryGirl.create(:user)
-			get "users/#{user.id}/sold_items"
+  describe "GET #sold_items" do
+		before { get :sold_items, id: user.id }
+
+		it "returns http success" do
       expect(response).to have_http_status(:success)
     end
+
+		it "responds with json" do
+			expect(response.content_type).to eq("application/json")
+		end
   end
 
 end
