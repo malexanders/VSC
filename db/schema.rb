8a4fedd2695e465e638a458068a05b02ab6c900c
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-15 14:42:42 -0400

    create categorization model to set up a has_many_through association for items and categories. This will replace the has_and_belongs_to many assocaition created previously. has_many_through_association is more flexible

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715182720) do
+ActiveRecord::Schema.define(version: 20160715184018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,11 @@ ActiveRecord::Schema.define(version: 20160715182720) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories_items", id: false, force: :cascade do |t|
-    t.integer "category_id"
-    t.integer "item_id"
+  create_table "categorizations", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "items", force: :cascade do |t|
---
+++ spec/factories/categorizations.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :categorization do
+    item_id 1
+    category_id 1
+  end
+end
