2cf2fafd8ea72d11e5f847df255279b083565a40
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-15 01:03:18 -0400

    set up the item status attribute as an ActiveRecord::Enum, I.E add enum status: {...} to the item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
 	belongs_to :seller, :class_name => 'User', :foreign_key => 'seller_id'
+	enum status: {available: 0, pending: 1, sold: 2, expired: 3, banned: 4 }
+
+	byebug
 end
