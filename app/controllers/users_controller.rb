c2f82a18ebe0148ee8bf198af9aee0f980a430b4
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-15 15:22:44 -0400

    users_controller, update seller_items action to return only items which have the status sold

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 
 	# Returns all items for which the selected user is the seller
+	# and the item has been sold.
 	def seller_items
-		User.find(params[:id]).seller_items
+		User.find(params[:id]).seller_items.sold
   end
 
 end
