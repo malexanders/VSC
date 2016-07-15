acdf4bfc36c92b2e105dd287b1ce2c004901e30a
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-15 17:16:36 -0400

    items_controller_spec, refactor; add spec in describe 'GET #index' to verify reponse is json

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe ItemsController, type: :controller do
-	describe "GET #items" do
+RSpec.describe ItemsController, type: [:controller] do
+
+	describe "GET #index" do
+		before { get :index }
 		it "returns http success" do
-			get :index
+			expect(response).to have_http_status(:success)
+		end
+
+		it "responds with json" do
+			expect(response.content_type).to eq("application/json")
+		end
+
+	end
+
+	describe "GET #show" do
+		it "returns http success" do
+			item = FactoryGirl.create(:item)
+			# get '/items/:id', id: item.id
+			get :show, id: item.id
 			expect(response).to have_http_status(:success)
 		end
 	end
+
 end
