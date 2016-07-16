1287a52075e7ae6a6625aed92020288d0251c5a3
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-15 21:46:04 -0400

    add category_with_items factory, and create category_with_items inside category_controller_spec

--- spec/controllers/category_controller_spec.rb
+++ spec/controllers/category_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CategoryController, type: :controller do
+let!(:category) { FactoryGirl.create(:category_with_items) }
 
   describe "GET #available_items" do
     it "returns http success" do
+			byebug
       get :available_items
       expect(response).to have_http_status(:success)
     end
--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
   factory :category do
     title "MyString"
+		
+		factory :category_with_item do
+			after(:create) do |category|
+				category.items << FactoryGirl.create(:item)
+			end
+		end
   end
 end
