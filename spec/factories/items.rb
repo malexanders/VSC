a8dcaac32c589fb53732ab1963fb3aa52f0fda9b
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-16 17:29:20 -0400

    factories/items, add association to user factory as seller

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
+	before
   factory :item do
-    title "MyString"
-    description "MyText"
-    price ""
-    status ""
+    title Faker::Commerce.product_name
+    description Faker::Lorem.paragraph
+    price Faker::Number.number(5)
+    status "sold"
     published_date "2016-07-14 23:47:48"
+		association :seller, factory: :user
   end
 end
