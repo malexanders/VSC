2561dcfc5e3bab4275bb993ed7f8a94eb3e08a14
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-15 20:40:24 -0400

    items_conttroller_spec, add spec to verify that #index returns all items

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe ItemsController, type: [:controller] do
-	let!(:item) { FactoryGirl.create(:item) }
+	let!(:item) { FactoryGirl.create_list(:item, 10) }
 
 	describe "GET #index" do
 		before { get :index }
@@ -13,10 +13,16 @@ RSpec.describe ItemsController, type: [:controller] do
 		it "responds with json" do
 			expect(response.content_type).to eq("application/json")
 		end
+
+		it "returns all items" do
+			parsed_body = JSON.parse(response.body)
+			all_items = Item.all
+			expect(parsed_body.length).to eq(all_items.length)
+		end
 	end
 
 	describe "GET #show" do
-		before {get :show, id: item.id}
+		before {get :show, id: item[0].id}
 
 		it "returns http success" do
 			expect(response).to have_http_status(:success)
