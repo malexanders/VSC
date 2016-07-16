5b1dbac4e5c4f1020ef86e5c6c9e3512840ec5d0
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-15 20:15:35 -0400

    factories\/users.rb, add factory user_with_items

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,12 @@ FactoryGirl.define do
     name "MyString"
     latitude "9.99"
     longtitude "9.99"
+
+		factory :user_with_item do
+			after(:create) do |user|
+				create_list(:item, 5, status: "sold", seller: user)
+			end
+		end
+
   end
 end
