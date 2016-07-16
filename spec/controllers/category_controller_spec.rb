95af6c9addb63e483b88b8522b0f5b89327207fa
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-15 21:47:43 -0400

    category_controller_spec, require spec_helper

--- spec/controllers/category_controller_spec.rb
+++ spec/controllers/category_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe CategoryController, type: :controller do
-let!(:category) { FactoryGirl.create(:category_with_items) }
+let!(:category) { FactoryGirl.create(:category_with_item) }
 
   describe "GET #available_items" do
     it "returns http success" do
-			byebug
       get :available_items
       expect(response).to have_http_status(:success)
     end
