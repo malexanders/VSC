94be050a6b5b089f57e462c3e7ef4c581875d368
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-17 11:40:05 -0400

    factories/users, refacctor factory 'user_with_items' to 'seller_with_items'

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     latitude '9.99'
     longtitude '9.99'
 
-    factory :user_with_items do
+    factory :seller_with_items do
       after(:create) do |user|
         create_list(:item, 5, status: 'sold', seller: user)
         create_list(:item, 15, status: 'available', seller: user)
