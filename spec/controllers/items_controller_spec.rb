1bc8bd30f2726bdd019f3219d9cac717d88c7358
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-17 00:03:18 -0400

    items_controller_spec, refactor and add comments

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -19,17 +19,25 @@ RSpec.describe ItemsController, type: [:controller] do
 			expect(response.content_type).to eq("application/json")
 		end
 
-		# Could make some improvements to this test
-		# Presently, it only checks if the same number of objects are returned
-		# From the db and the response
+		# Parses response.body
+		# Pulls all items from test db
+		# Iterates through response_items and db_items
+		# Extractes ids of all objects in each collection and passes them into separate arrays
+		# Expects the two arrays of ids to be equal
 		it "returns a list of all items" do
-			parsed_body = JSON.parse(response.body)
-			all_items = JSON.parse(Item.all.to_json)
-			expect(all_items.length).to eq(parsed_body.length)
+			response_items = JSON.parse(response.body)
+			db_items = JSON.parse(Item.all.to_json)
+
+			response_items_ids = response_items.map{|item| item["id"]}
+			db_items_ids = db_items.map{|item| item["id"]}
+
+			expect(parsed_body_ids).to eq(all_item_ids)
 		end
 	end
 
 	describe "GET #show" do
+
+
 		before {get :show, id: items[0].id}
 
 		it "returns http success" do
@@ -40,20 +48,24 @@ RSpec.describe ItemsController, type: [:controller] do
 			expect(response.content_type).to eq("application/json")
 		end
 
+		# Parses response.body
+		# Makes sure all required fields are present
+		# Verifies that response_item and db_item attributes match
+		# Had to manipulate the db_item attributes
+		# Because I manipulated some data in the jbuilder template
 		it "returns details for a specific item" do
-			returned_item = JSON.parse(response.body)
-			item = items[0]
-
-			expect(returned_item["id"]).to eq item.id
-			expect(returned_item["title"]).to eq item.title
-			expect(returned_item["description"]).to eq item.description
-			expect(returned_item["category"]).to eq item.categories.length > 0 ? item.categories.first.title : "none"
-			expect(returned_item["price"]).to eq item.price / 100.0
-			expect(returned_item["status"]).to eq item.status
-			expect(returned_item["published_date"]).to eq item.published_date.utc.to_s
-			expect(returned_item["seller_name"]).to eq item.seller.name
-			expect(returned_item["seller_latitude"]).to eq item.seller.latitude.to_f
-			expect(returned_item["seller_longtitude"]).to eq item.seller.longtitude.to_f
+			response_item = JSON.parse(response.body)
+			db_item = items[0]
+			expect(response_item["id"]).to eq db_item.id
+			expect(response_item["title"]).to eq db_item.title
+			expect(response_item["description"]).to eq db_item.description
+			expect(response_item["category"]).to eq db_item.categories.length > 0 ? item.categories.first.title : "none"
+			expect(response_item["price"]).to eq db_item.price / 100.0
+			expect(response_item["status"]).to eq db_item.status
+			expect(response_item["published_date"]).to eq db_item.published_date.utc.to_s
+			expect(response_item["seller_name"]).to eq db_item.seller.name
+			expect(response_item["seller_latitude"]).to eq db_item.seller.latitude.to_f
+			expect(response_item["seller_longtitude"]).to eq db_item.seller.longtitude.to_f
 
 		end
 	end
