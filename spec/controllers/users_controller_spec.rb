93fec3963b731f0fdce42d75a83261ee0d98e659
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-07-17 11:32:33 -0400

    users_controller_spec, add spec 'returns only items where status equals sold'

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 
 RSpec.describe UsersController, type: [:controller] do
   let!(:user) { FactoryGirl.create(:user_with_items) }
+	let(:json) { JSON.parse(response.body) }
 
   describe 'GET #sold_items' do
     before { get :sold_items, id: user.id }
@@ -15,10 +16,15 @@ RSpec.describe UsersController, type: [:controller] do
       expect(response.content_type).to eq('application/json')
     end
 
+		it 'returns only items where status equals sold' do
+			answer = json.all?{|item| item["status"] == "sold"}
+			expect(answer).to eq(true)
+		end
+
+
     it 'returns a list of all sold items for a particular seller' do
       parsed_body = JSON.parse(response.body)
       items_sold = Item.where(seller: user).sold
-
       expect(parsed_body.length).to eq(items_sold.length)
     end
   end
